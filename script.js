75f83e692410a1e235432deaf904b1a72e153bf7
Rajitha1990911 <rajitha.e@ethimale.lk>
2026-02-14 12:24:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*************************************************
  CONFIGURATION
 *************************************************/
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxZwptld-70kZ5X4qLJ-LrXoQd31mmmekl_mQ1XtKQYDN2Tcii0tJ4eD6O9bwDb-AP3/exec;
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzCtkLRWWCUZCQEKCIIY_tzRi6Z_mcXBhBYQLxEBKIM-qaH2Pd5NZ_hxB0dyjboxfmN/exec";
 
 /*************************************************
  FIELD DEFINITIONS
